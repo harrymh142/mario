e00ccaf8e5b49585cdf04a775e861c87e1065cf9
Your Name <you@example.com>
2019-12-05 21:34:08 +0100

    übungen

---
+++ wordcl.py
@@ -0,0 +1,14 @@
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.pyplot as plt
+
+
+with open("Alice_in_wonderland.txt") as f:
+    text = f.read()
+
+wordcloud = WordCloud(width=1920, height=1200)
+STOPWORDS.update(['said', 'illustration'])
+wordcloud.generate(text)
+plt.imshow(wordcloud)
+plt.axis('off')
+plt.show()
+
